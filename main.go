67f8a260839e09b42248be6ea918cf9c23246939
flewyui <flewyui@gmail.com>
2023-06-19 17:12:45 +0900

    enable formatter

--- main.go
+++ main.go
@@ -1,57 +1,58 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 
-  "github.com/gin-gonic/gin"
-  "github.com/jinzhu/gorm"
-  _ "github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
-  db := sqlConnect()
-  defer db.Close()
+	db := sqlConnect()
+	defer db.Close()
 
-  router := gin.Default()
-  router.LoadHTMLGlob("templates/*.html")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-  router.GET("/", func(ctx *gin.Context){
-    ctx.HTML(200, "index.html", gin.H{})
-  })
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
 
-  router.Run()
+	router.Run()
 }
 
 func sqlConnect() (database *gorm.DB) {
-  DBMS := "mysql"
-  USER := "go_test"
-  PASS := "password"
-  PROTOCOL := "tcp(db:3306)"
-  DBNAME := "go_database"
+	DBMS := "mysql"
+	USER := "go_test"
+	PASS := "password"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "go_database"
 
-  CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 
-  count := 0
-  db, err := gorm.Open(DBMS, CONNECT)
-  if err != nil {
-    for {
-      if err == nil {
-        fmt.Println("")
-        break
-      }
-      fmt.Print(".")
-      time.Sleep(time.Second)
-      count++
-      if count > 180 {
-        fmt.Println("")
-        fmt.Println("DB接続失敗")
-        panic(err)
-      }
-      db, err = gorm.Open(DBMS, CONNECT)
-    }
-  }
-  fmt.Println("DB接続成功")
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count > 180 {
+				fmt.Println("")
+				fmt.Println("DB接続失敗")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+	fmt.Println("DB接続成功")
 
-  return db
+	return db
 }
